511cbdfa9e3682752764929aec17caf102fab5fb
Pancho Rdz <biolfco@gmail.com>
2026-05-22 11:05:54 -0600

    Add files via upload

---
+++ data.js
@@ -0,0 +1,370 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-01",
+      "name": "01",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 2.1020618286944925,
+        "pitch": -0.01588859243587848,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.8467996430241245,
+          "pitch": 0.1722976251048678,
+          "rotation": 0,
+          "target": "1-02"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.804306630429541,
+          "pitch": 0.5531131136172203,
+          "title": "<font dir=\"auto\" style=\"vertical-align: inherit;\"><font dir=\"auto\" style=\"vertical-align: inherit;\">Isla Santiaguillo</font></font>",
+          "text": "<font dir=\"auto\" style=\"vertical-align: inherit;\"><font dir=\"auto\" style=\"vertical-align: inherit;\">!HOLA¡</font></font><div><font dir=\"auto\" style=\"vertical-align: inherit;\"><font dir=\"auto\" style=\"vertical-align: inherit;\"><br></font></font></div><div><font dir=\"auto\" style=\"vertical-align: inherit;\"><font dir=\"auto\" style=\"vertical-align: inherit;\">Estas en la Isla de Santiaguillo, una de las 6 islas del Parque Nacional Sistema Arrecifal Veracruzano.</font></font></div>"
+        }
+      ]
+    },
+    {
+      "id": "1-02",
+      "name": "02",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -1.0730116390360038,
+        "pitch": -0.13853395769742605,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.7112951057584116,
+          "pitch": 0.18234237394998942,
+          "rotation": 1.5707963267948966,
+          "target": "2-03"
+        },
+        {
+          "yaw": -0.7911777716336701,
+          "pitch": -0.38819250790867343,
+          "rotation": 6.283185307179586,
+          "target": "5-06"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.6672219370009422,
+          "pitch": 0.356193868618254,
+          "title": "<font dir=\"auto\" style=\"vertical-align: inherit;\"><font dir=\"auto\" style=\"vertical-align: inherit;\">Faro de la Isla de Santiaguillo</font></font>",
+          "text": "<font dir=\"auto\" style=\"vertical-align: inherit;\"><font dir=\"auto\" style=\"vertical-align: inherit;\">El faro de la Isla de Santiaguillo mide 38 metros de altura. Su luz se enciende cada 10 segundos, y se puede ver desde 22 millas nauticas de distancia. Es totalmente automatico y se alimenta de la luz del sol mediante fotoceldas.</font></font>"
+        }
+      ]
+    },
+    {
+      "id": "2-03",
+      "name": "03",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -0.21259669857535712,
+        "pitch": -0.10501767760932879,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.7733201726711574,
+          "pitch": 0.03780932643442192,
+          "rotation": 1.5707963267948966,
+          "target": "3-04"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-04",
+      "name": "04",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.037836548996137864,
+          "pitch": 0.16253959043422128,
+          "rotation": 3.141592653589793,
+          "target": "4-05"
+        },
+        {
+          "yaw": -0.7873958647158403,
+          "pitch": 0.10656150895010441,
+          "rotation": 1.5707963267948966,
+          "target": "6-07"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-05",
+      "name": "05",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.06967248451431907,
+        "pitch": 0.5235952884849517,
+        "fov": 1.856930178929328
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0242929184252283,
+          "pitch": 0.12362019151151671,
+          "rotation": 3.141592653589793,
+          "target": "3-04"
+        },
+        {
+          "yaw": -0.015370893015063558,
+          "pitch": -0.26357811966528466,
+          "rotation": 0,
+          "target": "7-08"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-06",
+      "name": "06",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 1.5173224622276846,
+        "pitch": 0.8417186575846856,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.37415851199899564,
+          "pitch": 0.33089865074425795,
+          "rotation": 3.141592653589793,
+          "target": "3-04"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.7666386509809247,
+          "pitch": 1.0771721266631467,
+          "title": "<font dir=\"auto\" style=\"vertical-align: inherit;\"><font dir=\"auto\" style=\"vertical-align: inherit;\">Arena de la Isla</font></font>",
+          "text": "<font dir=\"auto\" style=\"vertical-align: inherit;\"><font dir=\"auto\" style=\"vertical-align: inherit;\">El sustrato de la Isla de Santiaguillo en realidad no es arena, son trozos de esqueletos de coral que por las corrientes marinas se han acumulado formando la isla.</font></font>"
+        }
+      ]
+    },
+    {
+      "id": "6-07",
+      "name": "07",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -0.17486886250655687,
+        "pitch": -0.18942001978947332,
+        "fov": 1.856930178929328
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.29355849580848137,
+          "pitch": -0.7284536088793665,
+          "rotation": 3.141592653589793,
+          "target": "8-09"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.9486088000333428,
+          "pitch": 0.44952714225395596,
+          "title": "<font dir=\"auto\" style=\"vertical-align: inherit;\"><font dir=\"auto\" style=\"vertical-align: inherit;\">Arrecife Santiaguillo</font></font>",
+          "text": "<font dir=\"auto\" style=\"vertical-align: inherit;\"><font dir=\"auto\" style=\"vertical-align: inherit;\">El arrecife de Santiaguillo es muy importante por la variedad de especies de corales y peces que alberga, y por eso es una de las Zonas Nucleo del Parque Nacional Sistema Arrecifal Veracruzano</font></font>"
+        }
+      ]
+    },
+    {
+      "id": "7-08",
+      "name": "08",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 1.9221423035761642,
+        "pitch": 0.054348932589727283,
+        "fov": 1.856930178929328
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.1903096125230643,
+          "pitch": 0.4617464899056536,
+          "rotation": 3.141592653589793,
+          "target": "3-04"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-09",
+      "name": "09",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -0.9348011247026609,
+        "pitch": 0.7684141969030627,
+        "fov": 1.856930178929328
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.43039577460471,
+          "pitch": 0.32660571755000944,
+          "rotation": 3.141592653589793,
+          "target": "0-01"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.6975482279099054,
+          "pitch": 0.012997847177310007,
+          "title": "<font dir=\"auto\" style=\"vertical-align: inherit;\"><font dir=\"auto\" style=\"vertical-align: inherit;\">Ciudad de Veracruz</font></font>",
+          "text": "<font dir=\"auto\" style=\"vertical-align: inherit;\"><font dir=\"auto\" style=\"vertical-align: inherit;\">La Isla de Santiaguillo esta ubicada a unos 30 km al este de la ciudad y puerto de Veracruz</font></font>"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
